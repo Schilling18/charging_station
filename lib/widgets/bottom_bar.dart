9862f3dd75b2daf54fd8dbd41995b383fa8e555e
Schilling18 <chris.schilling@outlook.de>
2025-06-17 16:15:16 +0200

    Linter refactoring

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -2,18 +2,30 @@
 //
 // This file builds the BottomBar Widget.
 //
-// __version__ = "1.0.1"
+// __version__ = "2.0.0"
 //
 // __author__ = "Christopher Schilling"
-//
+
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 
+/// A widget that builds the BottomBar, typically displayed at the bottom of the screen.
+///
+/// The BottomBar contains three buttons (Favorites, Settings, Filter) that can be tapped to trigger corresponding actions.
+///
+/// [onFavoritesTap] - The callback function when the "Favorites" button is tapped.
+/// [onSettingsTap] - The callback function when the "Settings" button is tapped.
+/// [onFilterTap] - The callback function when the "Filter" button is tapped.
 class BottomBar extends StatelessWidget {
   final VoidCallback onFavoritesTap;
   final VoidCallback onSettingsTap;
   final VoidCallback onFilterTap;
 
+  /// Constructor for the BottomBar widget.
+  ///
+  /// [onFavoritesTap] - The function to be called when the "Favorites" button is tapped.
+  /// [onSettingsTap] - The function to be called when the "Settings" button is tapped.
+  /// [onFilterTap] - The function to be called when the "Filter" button is tapped.
   const BottomBar({
     super.key,
     required this.onFavoritesTap,
@@ -50,6 +62,12 @@ class BottomBar extends StatelessWidget {
     );
   }
 
+  /// Helper method to build a button for the BottomBar.
+  ///
+  /// [label] - The text label for the button.
+  /// [onPressed] - The callback function that will be executed when the button is pressed.
+  ///
+  /// Returns an ElevatedButton widget with the provided label and onPressed callback.
   Widget _buildBottomButton({
     required String label,
     required VoidCallback onPressed,
